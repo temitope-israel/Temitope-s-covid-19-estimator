7797bdc2c7626b027ab9c11b535ec0b978fcf13c
temitope-israel <omoniyitemitopeisrael@gmail.com>
2020-04-11 08:26:36 +0100

    My Assessment C

--- src/estimator.js
+++ src/estimator.js
@@ -1,49 +1,47 @@
-const covid19ImpactEstimator = (data) =>{
-
+const covid19ImpactEstimator = data => data;{
+    
   
-  return{
-    // INPUT DATA
-    data: {
-      reportedCases: 35.235533,
+      // INPUT DATA
+    data = {
+      reportedCases: 35.235533
 
 
-    },
+    };
 
-    //IMPACT OUTPUT DATA
-    impact: {
-      currentlyInfected: '',
-      infectionsByRequestedTime: ''
+    //  IMPACT OUTPUT DATA
+    impact = {
+        currentlyInfected: '',
+        infectionsByRequestedTime: ''
+  
+      },
+  
+      //  SEVEREIMPACT DATA
+      severeImpact = {
+        currentlyInfected: '',
+        infectionsByRequestedTime: ''
+  
+      }
+   
+  
+    //    Currently INfected for the Impact Object/Output
+    currentlyInfected = data.reportedCases * 10;
+    impact.currentlyInfected = data.reportedCases * 10;
+  
+  
+    //    Currently Infected for the severeImpact Output
+    currentlyInfectedSevereImpact = data.reportedCases * 50;
+    severeImpact.currentlyInfected = currentlyInfectedSevereImpact;
+  
+    //    Computation for infectionsByREquestedTime for iMPACT OUTPUT
+    infectionsByRequestedTime = Math.round(currentlyInfected * 512);
+    impact.infectionsByRequestedTime = infectionsByRequestedTime;
+  
+  
+    //    Computation for infectionsByREquestedTime for SEVEREiMPACT OUTPUT
+    severeImpactInfectionsByRequestedTime = Math.round(severeImpact.currentlyInfected * 512);
+    severeImpact.infectionsByRequestedTime = severeImpactInfectionsByRequestedTime;
 
-    },
-
-    //SEVEREIMPACT DATA
-    severeImpact: {
-      currentlyInfected: '',
-      infectionsByRequestedTime: ''
-
-    }
-};
-
-
-  //Currently INfected for the Impact Object/Output
-currentlyInfected = data.reportedCases * 10;
-impact.currentlyInfected = currentlyInfected;
-
-
-//Currently Infected for the severeImpact Output
-currentlyInfectedSevereImpact = data.reportedCases * 50;
-severeImpact.currentlyInfected = currentlyInfectedSevereImpact;
-
-//Computation for infectionsByREquestedTime for iMPACT OUTPUT
-infectionsByRequestedTime = Math.round(currentlyInfected * 512);
-impact.infectionsByRequestedTime = infectionsByRequestedTime;
-
-
-//Computation for infectionsByREquestedTime for SEVEREiMPACT OUTPUT
-severeImpactInfectionsByRequestedTime = Math.round(severeImpact.currentlyInfected * 512);
-severeImpact.infectionsByRequestedTime = severeImpactInfectionsByRequestedTime;
 
 }
 
-
-export default covid19ImpactEstimator;
+//export default covid19ImpactEstimator;
