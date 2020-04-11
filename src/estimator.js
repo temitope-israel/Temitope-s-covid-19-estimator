0c59d7a2d63c4e8262d468743d3418e997375ec0
temitope-israel <omoniyitemitopeisrael@gmail.com>
2020-04-11 05:32:06 +0100

    My Estimator Assessment - Challenge One

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,53 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+//const input = data;
+
+
+return{
+// INPUT DATA
+data: {
+    reportedCases: 35.235533,
+
+
+},
+
+//IMPACT OUTPUT DATA
+impact: {
+    currentlyInfected: '',
+    infectionsByRequestedTime: '',
+
+},
+
+// SEVEREIMPACT DATA
+severeImpact: {
+    currentlyInfected: '',
+    infectionsByRequestedTime: '',
+
+},
+};
+
+//Currently INfected for the Impact Object/Output
+currentlyInfected = data.reportedCases * 10;
+impact.currentlyInfected = currentlyInfected;
+
+
+//Currently Infected for the severeImpact Output
+currentlyInfectedSevereImpact = data.reportedCases * 50;
+severeImpact.currentlyInfected = currentlyInfectedSevereImpact;
+
+//Computation for infectionsByREquestedTime for iMPACT OUTPUT
+infectionsByRequestedTime = Math.round(currentlyInfected * 512);
+impact.infectionsByRequestedTime = infectionsByRequestedTime;
+
+
+//Computation for infectionsByREquestedTime for SEVEREiMPACT OUTPUT
+severeImpactInfectionsByRequestedTime = Math.round(severeImpact.currentlyInfected * 512);
+severeImpact.infectionsByRequestedTime = severeImpactInfectionsByRequestedTime;
+
+}
+
+/*var tope;
+tope = "God help me with this project";
+console.log(tope);*/
+
 
 export default covid19ImpactEstimator;
